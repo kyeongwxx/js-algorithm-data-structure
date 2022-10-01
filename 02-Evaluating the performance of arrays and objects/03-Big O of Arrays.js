805cf0c08a459385d84a131942edc3e52f27c025
kyeongwxx <kyeongwxx@gmail.com>
2022-10-01 16:47:27 +0900

    [Evaluating the performance of arrays and objects] 배열 안의 데이터 접근

---
+++ 02-Evaluating the performance of arrays and objects/03-Big O of Arrays.js
@@ -0,0 +1,6 @@
+let names = ["Michael", "Melissa", "Andrea"];
+
+// Insertion - It depends(배열 앞 작업(push, pop)이 배열 뒤 작업(shift, unshift)보다 항상 느리다)
+// Removal - It depends(배열 앞 작업(push, pop)이 배열 뒤 작업(shift, unshift)보다 항상 느리다)
+// Searching - O(n)
+// Access - O(1)
