c86bfc2bc47c81178e1852708ae007a60bfc6b16
kyeongwxx <kyeongwxx@gmail.com>
2022-10-01 16:59:20 +0900

    [Evaluating the performance of arrays and objects] 배열 메소드의 빅오

---
+++ 02-Evaluating the performance of arrays and objects/04-Big O of Array Operations.js
@@ -0,0 +1,9 @@
+// push - O(1)
+// pop - O(1)
+// shift - O(n) / need re-index
+// unshift - O(n) / need re-index
+// concat - O(n)
+// slice - O(n)
+// splice - O(n)
+// sort - O(n * log n)
+// forEach, map, filter, reduce etc - O(n)
