d5081e482102741dbe78c071e80456c16beea112
kyeongwxx <kyeongwxx@gmail.com>
2022-10-02 20:44:17 +0900

    [Problem Solving Patterns] 구체적 예시

---
+++ 03-Problem Solving Patterns/02-explore concrete examples.js
@@ -0,0 +1,15 @@
+// Write a function which takes in a string and returns counts of each character in the string.
+
+// Start with simple examples
+charCount("aaaa"); // {a: 4}
+charCount("hello"); // {h: 1, e: 1, l: 2, o: 1}
+
+// Progress to more complex examples
+("my phone number is 182763");
+("Hello hi");
+
+// Explore examples with empty inputs
+charCount("");
+
+// Explore examples with invalid inputs
+charCount(null);
