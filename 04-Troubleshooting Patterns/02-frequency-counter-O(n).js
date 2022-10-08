6bcb4453e2d0ba66f8706f90904a523b00189f3a
kyeongwxx <kyeongwxx@gmail.com>
2022-10-08 18:38:15 +0900

    [Programmers] 나머지가 1이 되는 수 찾기

--- 04-Troubleshooting Patterns/02-frequency-counter-O(n).js
+++ 04-Troubleshooting Patterns/02-frequency-counter-O(n).js
@@ -15,11 +15,9 @@ function same(arr1, arr2) {
 
   for(let val of arr1) {
     frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
-
   }
   for(let val of arr2) {
     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
-
   }
 
   for(let key in frequencyCounter1) {
