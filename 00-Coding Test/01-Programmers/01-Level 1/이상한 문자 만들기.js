8f2a12587dfee6b7cae13e4c19ab1bde3e8a8256
kyeongwxx <kyeongwxx@gmail.com>
2022-10-08 21:34:30 +0900

    [Programmers] 이상한 문자 만들기

---
+++ 00-Coding Test/01-Programmers/01-Level 1/이상한 문자 만들기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  s = s.split(" ");
+
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s[i].length; j++) {
+      if (j % 2 === 0) {
+        answer += s[i][j].toUpperCase();
+      } else {
+        answer += s[i][j].toLowerCase();
+      }
+    }
+    answer += " ";
+  }
+
+  return answer.slice(0, -1);
+}
