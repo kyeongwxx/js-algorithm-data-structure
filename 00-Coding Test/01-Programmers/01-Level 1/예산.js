d6db265584e1897f5c1421b849864f8d4c81d5a1
kyeongwxx <kyeongwxx@gmail.com>
2022-10-10 20:57:08 +0900

    [Programmers] 예산

---
+++ 00-Coding Test/01-Programmers/01-Level 1/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+
+  let sum = 0;
+  let result = 0;
+
+  for (let i = 0; i < d.length; i++) {
+    sum += d[i];
+    if (sum <= budget) {
+      result++;
+    }
+  }
+
+  return result;
+}
