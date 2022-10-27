38c2b381d8cdae135fb39d3e63505f04841298d6
kyeongwxx <kyeongwxx@gmail.com>
2022-10-27 11:44:52 +0900

    [Programmers] 신규 아이디 추천

--- 00-Coding Test/01-Programmers/01-Level 1/신규 아이디 추천.js
+++ 00-Coding Test/01-Programmers/01-Level 1/신규 아이디 추천.js
@@ -1,9 +1,9 @@
 function solution(new_id) {
   let answer = new_id
     .toLowerCase()
-    .replace(/[^\w-_.]/g, "")
-    .replace(/\.+/g, ".")
-    .replace(/^\.|\.$/g, "")
+    .replace(/[^\w\-\.]/g, "") // []: 내부 문자중 하나, [^]: 부정, \w: 알파벳, 숫자, 언더스코어, \-: -를 문자로 사용, \.: .을 문자로 사용
+    .replace(/[\.]{2,}/g, ".") // {n,}: 앞 문자 n번 이상 반복
+    .replace(/^\.|\.$/g, "")   // ^x: x로 문자열 시작, x$: x로 문자열 끝
     .replace(/^$/g, "a")
     .slice(0, 15)
     .replace(/\.$/, "");
