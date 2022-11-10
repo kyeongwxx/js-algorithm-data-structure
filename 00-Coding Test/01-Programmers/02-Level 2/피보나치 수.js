0e64b4eb8c4bdab8b9ac9e849c62f6cc7b671d40
kyeongwxx <kyeongwxx@gmail.com>
2022-11-10 18:22:21 +0900

    [Programmers] 피보나치 수

---
+++ 00-Coding Test/01-Programmers/02-Level 2/피보나치 수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = [];
+  
+    for (let i = 0; i <= n; i++) {
+      if (i === 0) answer.push(0);
+      if (i === 1) answer.push(1);
+      if (i >= 2) answer.push((answer[i - 1] + answer[i - 2]) % 1234567);
+    }
+  
+    return answer[n];
+  }
