8d95cc82b62296b31138fb313b6d47eadd8d8df3
kyeongwxx <kyeongwxx@gmail.com>
2022-12-08 22:26:00 +0900

    [Programmers] 특정 문자 제거하기

---
+++ 00-Coding Test/01-Programmers/00-Level 0/특정 문자 제거하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, letter) {
+  let answer = "";
+
+  for (let i = 0; i < my_string.length; i++) {
+    if (my_string[i] === letter) continue;
+    answer += my_string[i];
+  }
+
+  return answer;
+}
