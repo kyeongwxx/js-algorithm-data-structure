fef1d48ddbf63f880392559ab2481e97f5bb45e3
kyeongwxx <kyeongwxx@gmail.com>
2023-01-07 20:21:44 +0900

    [Programmers] 배열의 유사도

--- 00-Coding Test/01-Programmers/00-Level 0/배열의 유사도.js
+++ 00-Coding Test/01-Programmers/00-Level 0/배열의 유사도.js
@@ -1,6 +1,22 @@
-function solution(price) {
-  if (price >= 500000) return parseInt(price * 0.8);
-  if (price >= 300000) return parseInt(price * 0.9);
-  if (price >= 100000) return parseInt(price * 0.95);
-  return parseInt(price)
+function solution(s1, s2) {
+  let count = 0;
+  let longer;
+  let shorter;
+
+  if (s1.length >= s2.length) {
+    longer = s1;
+    shorter = s2;
+  }
+  if (s2.length >= s1.length) {
+    longer = s2;
+    shorter = s1;
+  }
+
+  for (let i = 0; i < longer.length; i++) {
+    if (longer.includes(shorter[i])) count++;
+  }
+
+  return count;
 }
+
+console.log(solution(['a', 'b', 'c'], ['com', 'b', 'd', 'p', 'c']));
