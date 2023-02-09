39f4afb65e49c00021587ab6e8b28b01ae893c2d
kyeongwxx <kyeongwxx@gmail.com>
2023-02-10 01:09:49 +0900

    [Programmers] 가위 바위 보

---
+++ 00-Coding Test/01-Programmers/00-Level 0/가위 바위 보.js
@@ -0,0 +1,10 @@
+function solution(rsp) {
+  return rsp
+    .split('')
+    .map((v) => {
+      if (v === '2') return '0';
+      if (v === '0') return '5';
+      if (v === '5') return '2';
+    })
+    .join('');
+}
